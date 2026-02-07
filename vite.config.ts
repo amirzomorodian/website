0a0fab755191bb2f8ef9b6b65245454aa223c5ab
amirzomorodian <155231937+amirzomorodian@users.noreply.github.com>
2026-02-07 08:24:23 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => {
+    plugins: [react()],
+    base: '/Website/',
     const env = loadEnv(mode, '.', '');
     return {
       server: {
